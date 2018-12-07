4534ba30e1410dd993dc1614f50f31fa9df25176
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-12-07 05:45:46 -0600

    fix(client): remove unused MovieLink import

--- client/components/season-list/index.js
+++ client/components/season-list/index.js
@@ -2,7 +2,6 @@ import {Link} from 'react-router-dom'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import MovieLink from '../movie-link/'
 import React from 'react'
 
 export default class extends React.Component {
