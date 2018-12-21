7b66a2e9b8b94ace225f1b6470d1f511738453c2
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-12-21 06:41:39 -0600

    test(client/episode-list): add tests

---
+++ client/components/episode-list/index.test.js
@@ -0,0 +1,35 @@
+import EpisodeList from '.'
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+test('EpisodeList displays list of episodes', () => {
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      shows: [
+        {name: 'Episode 1', show: '/show/season1/episode1'},
+        {name: 'Episode 2', show: '/show/season1/episode2'}
+      ]
+    })
+  )
+
+  const props = {
+    match: {
+      params: {
+        season: 'seasonname',
+        showname: 'showname'
+      }
+    }
+  }
+
+  const element = <EpisodeList {...props} />
+
+  const component = renderer.create(element)
+  expect(component.toJSON()).toMatchSnapshot()
+
+  return new Promise(resolve => setTimeout(resolve, 0)).then(() => {
+    component.update(element)
+    expect(component.toJSON()).toMatchSnapshot()
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(fetch.mock.calls[0][0]).toEqual('/tv/showname/seasonname')
+  })
+})
